8c7043aa55560ab06a44b507e8981ededbe04399
atreluted <atreluted@proton.me>
2026-06-20 15:05:48 -0600

    Update dllmain.cpp

--- heartbeatinj/module/mdopduel/dllmain.cpp
+++ heartbeatinj/module/mdopduel/dllmain.cpp
@@ -1,5 +1,29 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include <Windows.h>
+#include <iostream>
+#include <thread>
+
+#define REBASE(x) x + (uintptr_t)GetModuleHandle(nullptr)
+
+void start()
+{
+    const uintptr_t printoffs = REBASE(0x1EDBA40);
+
+    typedef enum { print, info, warn, error } printtype;
+    using rprint = void(__fastcall*)(printtype type, const char* fmt, ...);
+    auto rblxprint = (rprint)printoffs;
+
+    int timer = 1;
+
+    while (true)
+    {
+        rblxprint(print, "injected for %d seconds", timer);
+        timer++;
+        Sleep(1000);
+    }
+}
+
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -9,7 +33,9 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(hModule);
         MessageBoxA(NULL, "LAMSDOQDASFASFGD", "LAMSDOQWEFDA", NULL);
+        //std::thread(start).detach();
         break;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
